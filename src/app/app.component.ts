ae3b6865c2ef06653fd892915cb0cb121bf103a2
ashshukl <ashishshukla321@hotmail.com>
2023-08-23 16:56:31 +0530

    Updated
    Updated

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Observable, Observer, Subscription, map, merge, of, tap, pipe } from 'rxjs';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'ObservableProj';
+  studentList = ['Ashish', 'Jelly', 'Priya', 'Kavita', 'Mahesh']
+  studentsO: Observable<string[]> = of(this.studentList);
+  stds:string[] = [];
+  
+  
+  ngOnInit(): void {
+    // this.students.subscribe((data) => { this.stds = data });
+    const subscription = this.studentsO.subscribe({
+      next: (data) => {this.stds = data},
+      error: () => console.log('error occured')
+    }
+    )
+
+    setTimeout(() => {
+      subscription.unsubscribe();
+    }, 5000);
+  }
+
+  btnClick(){
+      this.studentList.push('Hanu');    
+      console.log(this.studentList);
+  }
 }
